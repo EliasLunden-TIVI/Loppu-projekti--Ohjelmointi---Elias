224d5d4c8549dcc815ccbe01229fd3c26976f6ad
EliasLunden-TIVI <53103@edu.raseko.fi>
2025-03-28 10:33:20 +0200

    Phase 3. Additions and QOL. Fixed Generation

--- Password-Generator.py
+++ Password-Generator.py
@@ -1,71 +1,80 @@
 #// Aloitettu 8:05 / 28.3.2025
-#// Prewarm the libraries
+#// Perustaa kirjaston random
 import random
 
-#//Prewarm the lists
-ranletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"] #Ascii lowercase - Default
-ranupperletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"] #Ascii Uppercase 
-rannumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] #Ascii numbers 
-rannordics = ["ä", "Ä", "ö", "Ö", "å", "Å"] #Ascii nordics 
-ranspecial = ["!", "#", "¤", "%", "&", "/", "=", "+", "-"] #Ascii special
-#//Welcome message//#
-print("Welcome to the password generator!")
+#//Perustetaan Listat
+chosenletters = []
+ranlowerletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"] #Ascii pienent kirjaimet - Default valinta. Pakollinen.
+ranupperletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"] #Ascii isot kirjaimet 
+rannumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] #Ascii numerot 
+rannordics = ["ä", "Ä", "ö", "Ö", "å", "Å"] #Ascii ääkköset 
+ranspecial = ["!", "#", "¤", "%", "&", "/", "=", "+", "-"] #Ascii erikoismerkit
+#//Tervetuloviesti//#
+print("Tervetuloa satunnais-salasanan luojaan")
 
+#//Valitse toivotut merkit ja kirjaimet.
+print("Aloitetaan mukautus:")
 
+#// Pienet kirjaimet
+lowers = input("Haluatko pieniä kirjaimia salasanaan? 0 = Ei 1 = Kyllä: ")
 
-#//Choose the avaivable letters
-print("Starting configuration:")
+if lowers == str(0): #// Ei
+    print("Ei pieniä kirjaimia")
+elif lowers == str(1): #// Kyllä
+    print("Sisältää pieniä krijaimia")
+    chosenletters = chosenletters + ranlowerletters
+else: #// Backup-Failsafe (Palauttaa aina nolla / ei)
+    print("Ei hyväksytty syöte. Palautetaan valinta: EI.")
+#// Isot kirjaimet 
+uppers = input("Haluatko isoja kirjaimia salasanaan? 0 = Ei 1 = Kyllä: ")
 
-#// Uppercase letters
-uppers = input("Add uppercase lettters to generation? 0 = No 1 = Yes: ")
+if uppers == str(0): #// Ei
+    print("Ei isoja kirjaimia")
+elif uppers == str(1): #// Kyllä
+    print("Lisätään isoja kirjaimia")
+    chosenletters = chosenletters + ranupperletters
+else: #// Backup-Failsafe (Palauttaa aina nolla / ei)
+    print("Ei hyväksytty syöte. Palautetaan valinta: EI.")
 
-if uppers == str(0): #// If no uppercase
-    print("No uppercase")
-if uppers == str(1): #// If Yes uppercase
-    print("Adding Uppercaseletters")
-    ranletters.append(ranupperletters)
-
-else: #// Backup-Failsafe (Allways defaults to 0 / no uppercase)
-    print("Invalid input. Defaulting to no uppercase.")
-
-#// Number letters
+#// Numero kirjaimet
 numbers = input("Add numbers 0-9 to generation? 0 = No 1 = Yes: ")
 
-if numbers == str(0): #// If no numbers
-    print("No numbers")
-if numbers == str(1): #// If Yes numbers
-    print("Adding numbers")
-    ranletters.append(rannumbers)
+if numbers == str(0): #// Ei
+    print("Ei numeroita")
+elif numbers == str(1): #// Kyllä
+    print("Lisätään numeroita")
+    chosenletters = chosenletters + rannumbers
+else: #// Backup-Failsafe (Palauttaa aina nolla / ei)
+    print("Ei hyväksytty syöte. Palautetaan valinta: EI.")
 
-else: #// Backup-Failsafe (Allways defaults to 0 / no numbers)
-    print("Invalid input. Defaulting to no numbers.")
-
-#//Specials
+#//Erikois merkit
 specials = input("Add special charecters? #, !, %: 0 = No 1 = Yes: ")
 
-if specials == str(0): #// If No
-    print("No special charecters.")
-if numbers == str(1): #// If Yes
-    print("Adding special charecters")
-    ranletters.append(rannordics)
+if specials == str(0): #// Ei
+    print("Ei erikoismerkkejä")
+elif numbers == str(1): #// Kyllä
+    print("Lisätään erikoismerkkejä")
+    chosenletters = chosenletters + ranspecial
+else: #// Backup-Failsafe (Palauttaa aina nolla / ei)
+    print("Ei hyväksytty syöte. Palautetaan valinta: EI.")
 
-else: #// Backup-Failsafe (Allways defaults to 0 / no special charecters)
-    print("Invalid input. Defaulting to no special charecters.")
+nordics = input("Haluatko Pohjoismaan kirjaimet (ÄÄKÖSET)? Ä, Ö, Å: 0 = No 1 = Yes: ")
 
-nordics = input("Add nordic lettering? Ä, Ö, Å: 0 = No 1 = Yes: ")
+if nordics == str(0): #// Ei
+    print("Ei ÄÄKKÖ:siä.")
+elif numbers == str(1): #// Kyllä
+    print("Lisätään ÄÄKKÖ:set")
+    chosenletters = chosenletters + rannordics
+else: #// Backup-Failsafe (Palauttaa aina nolla / ei)
+    print("Ei hyväksytty syöte. Palautetaan valinta: EI.")
 
-if nordics == str(0): #// If No
-    print("No nordic lettering.")
-if numbers == str(1): #// If Yes
-    print("Adding numbers")
-    ranletters.append(rannordics)
+#// Salasanan pituus
 
-else: #// Backup-Failsafe (Allways defaults to 0 / no nordics)
-    print("Invalid input. Defaulting to no nordics.")
+print("DEBUG! Valitut kirjaimet" , chosenletters)
 
+passwordLength = int(input("Input desired lenght : "))
 
-#// Pasword lenght
-passwordLenght = input("Enter number value for password lenght: ")
+password = "".join(random.choice(chosenletters) for _ in range(passwordLength)) #// Syöttää muuttujaan password satunnaisen kirjainsarjan, ja leikkaa sen sopivan pituiseksi Password-lenght parametrilla."
 
+print("Salasanasi on:" , password) #//Tulostaa salasanan käyttäjälle.
 
-print(ranletters)
