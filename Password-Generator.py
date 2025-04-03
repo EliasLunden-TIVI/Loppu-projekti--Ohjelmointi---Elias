da492c450450dba8799e5264ceeed1bf88428d80
N3xu_ss <53103@edu.raseko.fi>
2025-04-03 08:54:40 +0300

    1.2 Randomisaatio parannukset, koodin tiivistys ja muuta!

--- Password-Generator.py
+++ Password-Generator.py
@@ -3,85 +3,63 @@
 import random
 
 #//Perustetaan Listat
-chosenletters = [] #// Valitut merkit tallentuu tähän. Käytetään rivillä 84.
-ranlowerletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"] #Ascii pienent kirjaimet - Default valinta. Pakollinen.
-ranupperletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"] #Ascii isot kirjaimet 
-rannumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] #Ascii numerot 
-rannordics = ["ä", "Ä", "ö", "Ö", "å", "Å"] #Ascii ääkköset 
-ranspecial = ["!", "#", "¤", "%", "&", "/", "=", "+", "-", "@"] #Ascii erikoismerkit
-ransentence = ["Iloinen", "Surullinen", "Panda", "Kettu", "Orava", "Kissa", "Koira", "Possu", "Lehmä", "Lammas", "Punainen", "Sininen", "Keltainen", "Vihreä", "Oranssi", "Pinkki", "Musta", "Valkoinen", ""] #Lauselista
+chosenletters = ""  # Valitut merkit tallentuu tähän. Käytetään rivillä 84.
+ranlowerletters = "abcdefghijklmnopqrstuvwxyz"  # Ascii pienet kirjaimet - Default valinta. Pakollinen.
+ranupperletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  # Ascii isot kirjaimet 
+rannumbers = "0123456789"  # Ascii numerot 
+rannordics = "äÄöÖåÅ"  # Ascii ääkköset 
+ranspecial = "!#¤%&/=+-@"  # Ascii erikoismerkit
+ransentence = [
+    "Iloinen", "Surullinen", "Panda", "Kettu", "Orava", "Kissa", "Koira", "Possu",
+    "Lehmä", "Lammas", "Punainen", "Sininen", "Keltainen", "Vihreä", "Oranssi",
+    "Pinkki", "Musta", "Valkoinen"
+]
 #//Tervetuloviesti//#
 print("Tervetuloa satunnais-salasanan luojaan")
 
 #//Valitse toivotut merkit ja kirjaimet.
 print("Aloitetaan mukautus:")
 
-#// Pienet kirjaimet
-lowers = input("Haluatko pieniä kirjaimia salasanaan? 0 = Ei 1 = Kyllä: ")
-#//If-else
-if lowers == str(0): #// Ei
-    print("Ei pieniä kirjaimia")
-elif lowers == str(1): #// Kyllä
-    print("Sisältää pieniä krijaimia")
-    chosenletters = chosenletters + ranlowerletters
-else: #// Backup-Failsafe (Palauttaa aina nolla / ei)
-    print("Ei hyväksytty syöte. Palautetaan valinta: EI.")
-#// Isot kirjaimet 
-uppers = input("Haluatko isoja kirjaimia salasanaan? 0 = Ei 1 = Kyllä: ")
-#//If-else
-if uppers == str(0): #// Ei
-    print("Ei isoja kirjaimia")
-elif uppers == str(1): #// Kyllä
-    print("Lisätään isoja kirjaimia")
-    chosenletters = chosenletters + ranupperletters
-else: #// Backup-Failsafe (Palauttaa aina nolla / ei)
-    print("Ei hyväksytty syöte. Palautetaan valinta: EI.")
+#//Alustaa kysymis funktion, joka palauttaa arvon kyllä / ei. Joka taas päättää lisätäänkö esitetty merkkijono listaan.//#
+def kysy_ja_lisaa(viesti, merkkijoukko):
+    vastaus = input(viesti + " 0 = Ei 1 = Kyllä: ") #Kysymys dialoogi
+    if vastaus == "1":
+        print(f"Lisätään" , {viesti})
+        return merkkijoukko
+    elif vastaus == "0":
+        print(f"Ei" , {viesti})
+        return ""
+    else:
+        print("Ei hyväksytty syöte. Palautetaan valinta: EI.") #Failsafe
+        return ""
 
-#// Numero kirjaimet
-numbers = input("Haluatko numeroita 0 - 9 salasanaan? 0 = Ei 1 = Kyllä: ")
-#//If-else
-if numbers == str(0): #// Ei
-    print("Ei numeroita")
-elif numbers == str(1): #// Kyllä
-    print("Lisätään numeroita")
-    chosenletters = chosenletters + rannumbers
-else: #// Backup-Failsafe (Palauttaa aina nolla / ei)
-    print("Ei hyväksytty syöte. Palautetaan valinta: EI.")
+#//Alustaa listan josta merkit poimitaan//#
+chosenletters = ""
 
-#//Erikois merkit
-specials = input("Haluatko erikoismerkkejä salaasnaan? #, !, %: 0 = Ei 1 = Kyllä: ")
-#//If-else
-if specials == str(0): #// Ei
-    print("Ei erikoismerkkejä")
-elif numbers == str(1): #// Kyllä
-    print("Lisätään erikoismerkkejä")
-    chosenletters = chosenletters + ranspecial
-else: #// Backup-Failsafe (Palauttaa aina nolla / ei)
-    print("Ei hyväksytty syöte. Palautetaan valinta: EI.")
-#//ÄÄKÖSET
-nordics = input("Haluatko Pohjoismaan kirjaimet (ÄÄKÖSET)? Ä, Ö, Å: 0 = Ei 1 = Kyllä: ")
-#//If-else
-if nordics == str(0): #// Ei
-    print("Ei ÄÄKKÖ:siä")
-elif numbers == str(1): #// Kyllä
-    print("Lisätään ÄÄKKÖ:set")
-    chosenletters = chosenletters + rannordics
-else: #// Backup-Failsafe (Palauttaa aina nolla / ei)
-    print("Ei hyväksytty syöte. Palautetaan valinta: EI.")
-#//SANAT
-sanat = input("Haluatko salasanaan satunnaisia sanoja? Kissa, Koira: 0 = Ei 1 = Kyllä: ")
-#//If-else
-if sanat == str(0): #// Ei
-    print("Ei sanoja")
-elif sanat == str(1): #// Kyllä
-    print("Lisätään sanoja")
-    chosenletters = chosenletters + ransentence
-else: #// Backup-Failsafe (Palauttaa aina nolla / ei)
-    print("Ei hyväksytty syöte. Palautetaan valinta: EI.")
+#//Kysymis funktiot//#
+chosenletters += kysy_ja_lisaa("pieniä kirjaimia", ranlowerletters)
+chosenletters += kysy_ja_lisaa("isoja kirjaimia", ranupperletters)
+chosenletters += kysy_ja_lisaa("numeroita", rannumbers)
+chosenletters += kysy_ja_lisaa("erikoismerkkejä", ranspecial)
+chosenletters += kysy_ja_lisaa("pohjoismaisia kirjaimia (Ä, Ö, Å)", rannordics)
+
+#// Kysyy erikseen toivooko käyttäjä satunnaisia lauseita salasanaan
+
+ransentenceKYS = input("Haluatko satunnaisia sanoja salasanaan? (Kettu, Punainen)" )
+if ransentenceKYS == "1":
+    print(f"Lisätään satunnaisia sanoja")
+    chosenletters
+elif ransentenceKYS == "0":
+    print(f"Ei lisätä satunnaisia sanoja")
+else:
+    print("Ei hyväksytty syöte. Palautetaan valinta: EI.") #Failsafe
+    ransentenceKYS == "0"
 
 #// Salasanan pituus
 
-#print("DEBUG! Valitut kirjaimet" , chosenletters)
+print("DEBUG! Valitut kirjaimet" , chosenletters)
+if ransentence == "1":
+    print(ransentence)
 jatkaminen = True #//valmistaa Boolean lauseen.
 
 #//Jatkamis looppi
@@ -93,6 +71,14 @@ while jatkaminen == True:
         passwordLength = 10
 
     password = "".join(random.choice(chosenletters) for _ in range(passwordLength)) #// Syöttää muuttujaan password satunnaisen kirjainsarjan, ja leikkaa sen sopivan pituiseksi Password-lenght parametrilla."
+
+    #// Lisää satunnaisen sanan 
+    if ransentenceKYS == "1":
+        ranWord = random.choice(ransentence)
+        insert_position = random.randint(0, len(password))  #//Valitsee kohdan sanan lisäämiseen salasanan indeksistä.
+        password = password[:insert_position] + ranWord + password[insert_position:]
+    else:
+        print("Ei lisätä sanoja:")
 
     print("Salasanasi on:" , password) #//Tulostaa salasanan käyttäjälle.
 
